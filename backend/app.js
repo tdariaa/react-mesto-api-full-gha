aa505bd9b6c9cee8d8f5c4fc3f73fdc6aee90f7d
Dasha Tarasova <tdariaa@mail.ru>
2023-08-28 01:39:15 +0300

    port 3000

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const {
   signupValidation,
 } = require('./middlewares/celebrate-validation');
 
-const { PORT = 4000, DB_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
 const app = express();
 
 const limiter = rateLimit({
